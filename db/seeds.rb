f500b4480d75453f3fa2556c4b1008d10ec1da73
annedien <annedient@gmail.com>
2016-10-13 13:18:22 +0200

    Create Photo: Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.delete_all
-Room.delete_all
+Photo.delete_all
 Theme.delete_all
+Room.delete_all
+User.delete_all
 
 modern_and_minimal = Theme.create(name: "Modern and Minimal")
 sleek_and_sophisticated = Theme.create(name: "Sleek and Sophisticated")
@@ -18,12 +19,20 @@ a_mans_touch = Theme.create(name: "A Man's Touch")
 white_washed = Theme.create(name: "White Washed")
 tempting_terrace = Theme.create(name: "Tempting Terrace")
 
+#users
 miriam = User.create(email: "miriam@codaisseurbnb.com", password: "abcd1234")
 wouter = User.create(email: "wouter@codaisseurbnb.com", password: "abcd1234")
 matt = User.create(email: "annedien@gmail.com", password: "1234")
 
+# Photos
+photo1 = Photo.create(remote_image_url: "http://res.cloudinary.com/annedien/image/upload/v1476356657/bellagio-hotel-resort-room-king-sapphire.tif.image.300.300.high_rt51ob.jpg")
+photo2 = Photo.create(remote_image_url: "http://res.cloudinary.com/annedien/image/upload/v1476356632/alluring-design-hotel-room-interior-ideas-peach-wall-paint-color-white-wooden-covered-bedding-sheets-brown-wooden-bedside-tables-luxury-bedrooms-188-best-luxury-bedroom-interior-design-gallery-best-300x300_x7xpqv.jpg")
+photo3 = Photo.create(remote_image_url: "http://res.cloudinary.com/annedien/image/upload/v1476356607/mirage-hotel-architectural-resort-tower-queen.tif.image.300.300.high_dzg53k.jpg")
+
+#rooms
 Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2, bathroom_count: 3, listing_name: "You will never forget your stay!", description: "Normcore meh put a bird on it man braid mumblecore, locavore synth swag kogi helvetica ugh health goth twee raw denim readymade.", address: "Colon 58, 1B, 11100 Cadiz (Spain)", has_tv: false, has_kitchen: true, has_airco: false, has_heating: false, has_internet: true, price: 100, active: true, user: miriam, themes: [modern_and_minimal, sleek_and_sophisticated])
 
 Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2, bathroom_count: 3, listing_name: "A place in the middle of Holland", description: "Freegan yuccie everyday carry tote bag you probably haven't heard of them godard.", address: "Daguerrestraat 23, 1054TG Utrecht (Netherlands)", has_tv: false, has_kitchen: true, has_airco: false, has_heating: false, has_internet: true, price: 100, active: true, user: wouter, themes: [warm_and_cozy, a_mans_touch])
 
 Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2, bathroom_count: 3, listing_name: "Great for a Weekend", description: "Meditation tattooed lumbersexual street art truffaut pour-over, kale chips pitchfork. Readymade snackwave man bun, photo booth tilde wolf occupy fashion axe VHS authentic PBR&B typewriter. Vape everyday carry meditation try-hard hammock unicorn.", address: "Tottenham Court Road 14, W1T 1JY London (UK)", has_tv: false, has_kitchen: true, has_airco: false, has_heating: false, has_internet: true, price: 100, active: true, user: matt, themes: [elegant_and_ornate, white_washed])
+
