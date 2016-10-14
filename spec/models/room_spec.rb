6da6b2bd6276a3ac2f50ae539d766c2cba350889
annedien <annedient@gmail.com>
2016-10-14 13:19:27 +0200

    implement range overlapping algorithm

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+
   describe "validations" do
     it "is invalid without a home_type" do
       room = Room.new(home_type: "")
@@ -33,9 +34,10 @@ RSpec.describe Room, type: :model do
   end
 
   describe "#order_by_price" do
-    let!(:room1) { create :room, price: 100 }
-    let!(:room2) { create :room, price: 200 }
-    let!(:room3) { create :room, price: 300 }
+    let(:user) { create :user }
+    let!(:room1) { create :room, price: 100, user:user}
+    let!(:room2) { create :room, price: 200, user:user }
+    let!(:room3) { create :room, price: 300, user:user }
 
     it "returns a sorted array of rooms by prices" do
       expect(Room.order_by_price).to match_array [room1, room2, room3]
@@ -51,6 +53,32 @@ RSpec.describe Room, type: :model do
 
     it "has guests" do
       expect(room.guests).to include(guest_user)
+    end
+  end
+
+
+  describe "#available?" do
+    let(:host_user) { create :user, email: "host@user.com" }
+    let(:guest_user) { create :user, email: "guest@user.com" }
+
+    let(:room) { create :room, price: 20, user: host_user }
+
+    let!(:existent_booking) { create :booking, room: room, starts_at: 2.days.from_now, ends_at: 6.days.from_now, user: guest_user }
+
+    context "is available" do
+      subject { room.available?(8.days.from_now, 10.days.from_now) }
+
+      it "returns true" do
+        expect(subject).to be true
+      end
+    end
+
+    context "is not available" do
+      subject { room.available?(4.days.from_now, 10.days.from_now) }
+
+      it "returns false" do
+        expect(subject).to be false
+      end
     end
   end
 end
